e14277609b5f6bec526ec4d280028d3849817e93
adilsonchameia <adilsonkchameia@gmail.com>
2021-03-20 13:39:11 +0100

    Criacao da home page

---
+++ lib/Principal.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bitcoin"),
+        backgroundColor: Colors.orange,
+        toolbarHeight: 30,
+        toolbarOpacity: 0.6,
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
